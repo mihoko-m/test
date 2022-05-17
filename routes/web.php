66aaf5431f22cfa820adbaf9e9260a884f4b4108
mihoko-m <mihoko@aem777.com>
2022-05-17 22:40:34 +0900

    ルーティング順番変更

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@
 
 Route::get('/', 'PostController@index');
 
-Route::get('/posts/{post}','PostController@show');
-
 Route::get('/posts/create', 'PostController@create');
 
 Route::post('/posts', 'PostController@store');
+
+Route::get('/posts/{post}','PostController@show');
+
